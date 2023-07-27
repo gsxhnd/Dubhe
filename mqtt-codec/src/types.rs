308d5f1f7c67cd834c76cac10ab5f26c464571e7
gsxhnd <gsxhnd@gmail.com>
2023-07-27 13:09:52 +0800

    update

--- mqtt-codec/src/types.rs
+++ mqtt-codec/src/types.rs
@@ -121,7 +121,7 @@ pub enum ProtocolError {
     _MalformedPacket(DecodeError),
     _ConnectTimedOut,
     _FirstPacketNotConnect,
-    _InvalidProtocolName,
+    InvalidProtocolName,
     _KeepAliveTimeout,
 }
 
@@ -161,33 +161,7 @@ pub enum PacketType {
     AUTH = 0b1111_0000,
 }
 
-#[derive(Debug, Clone)]
-pub struct ConnectPacket {
-    // Variable Header
-    pub protocol_name: String,
-    pub protocol_version: ProtocolVersion,
-    pub clean_start: bool,
-    pub keep_alive: u16,
-
-    // Properties
-    // pub session_expiry_interval: Option<SessionExpiryInterval>,
-    // pub receive_maximum: Option<ReceiveMaximum>,
-    // pub maximum_packet_size: Option<MaximumPacketSize>,
-    // pub topic_alias_maximum: Option<TopicAliasMaximum>,
-    // pub request_response_information: Option<RequestResponseInformation>,
-    // pub request_problem_information: Option<RequestProblemInformation>,
-    // pub user_properties: Vec<UserProperty>,
-    // pub authentication_method: Option<AuthenticationMethod>,
-    // pub authentication_data: Option<AuthenticationData>,
-
-    // Payload
-    pub client_id: String,
-    // pub will: Option<FinalWill>,
-    pub user_name: Option<String>,
-    pub password: Option<String>,
-}
-
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum ProtocolVersion {
     MQTT3,
     MQTT4,
