eab984bf16afe0a91e5ca214ac083ea0b88c1a9c
gsxhnd <gsxhnd@gmail.com>
2023-08-13 01:04:50 +0800

    init web

---
+++ dubhe-web/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import { ref, onMounted, onUnmounted } from "vue";
+import { testStore } from "@/stores/index";
+
+const test = testStore();
+</script>
+
+<template>
+  <router-view class="router-view" />
+</template>
+
+<style scoped lang="less"></style>
---
+++ dubhe-web/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="Hello">hello world</div>
+</template>
+
+<style scoped lang="less"></style>
