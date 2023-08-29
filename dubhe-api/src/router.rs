b1f16ebab12177abd7fb11ea7bf0f4a867b6f208
gsxhnd <gsxhnd@gmail.com>
2023-08-29 20:02:14 +0800

    router refactor

--- dubhe-api/src/router.rs
+++ dubhe-api/src/router.rs
@@ -1,11 +1,60 @@
-use axum::routing::get;
-use axum::Router;
+use axum::{
+    http::{header, StatusCode, Uri},
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use rust_embed::RustEmbed;
 
-pub fn api_router() -> Router {
-    Router::new().route("/", get(root))
+static INDEX_HTML: &str = "index.html";
+
+#[derive(RustEmbed)]
+#[folder = "../dubhe-web/dist/"]
+struct Assets;
+
+pub fn api_router(web_enable: bool) -> Router {
+    let mut r = Router::new().route("/api/v1/hello", get(root));
+
+    if web_enable {
+        r = r.fallback(static_handler);
+    }
+    return r;
 }
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn static_handler(uri: Uri) -> impl IntoResponse {
+    let path = uri.path().trim_start_matches('/');
+
+    if path.is_empty() || path == INDEX_HTML {
+        return index_html().await;
+    }
+
+    match Assets::get(path) {
+        Some(content) => {
+            let mime = mime_guess::from_path(path).first_or_octet_stream();
+
+            ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+        }
+        None => {
+            // if path.contains('.') {
+            //     return not_found().await;
+            // }
+            // index_html().await
+            not_found().await
+        }
+    }
+}
+async fn index_html() -> Response {
+    match Assets::get(INDEX_HTML) {
+        Some(content) => Html(content.data).into_response(),
+        None => not_found().await,
+    }
+}
+
+async fn not_found() -> Response {
+    (StatusCode::NOT_FOUND, "404").into_response()
 }
--- dubhe-api/src/server.rs
+++ dubhe-api/src/server.rs
@@ -1,10 +1,3 @@
-use axum::{
-    self,
-    http::{header, StatusCode, Uri},
-    response::{Html, IntoResponse, Response},
-    Router,
-};
-use rust_embed::RustEmbed;
 use std::net::SocketAddr;
 use tracing::info;
 
@@ -13,64 +6,30 @@ use crate::router;
 
 pub struct ApiServer {
     cfg: ApiConfig,
+    addr: SocketAddr,
 }
-
-static INDEX_HTML: &str = "index.html";
-
-#[derive(RustEmbed)]
-#[folder = "../dubhe-web/dist/"]
-struct Assets;
 
 impl ApiServer {
     pub fn new(cfg: ApiConfig) -> ApiServer {
-        ApiServer { cfg }
+        let addr: SocketAddr = cfg.listener_addr.parse().expect("msg");
+        ApiServer { cfg, addr }
     }
     pub async fn run(&self) {
-        let addr: SocketAddr = self.cfg.listener_addr.parse().expect("msg");
+        let api_router = router::api_router(self.cfg.web.enable);
+        self.output().await;
 
-        let api_router = router::api_router();
-        let mut app = Router::new().nest("/api/v1/hello", api_router);
-        if self.cfg.web.enable {
-            app = app.fallback(static_handler);
-            info!("api service web enable, listened in: http://{}", addr);
-        }
-
-        info!("api service enable, listened in: http://{}/api/v1", addr);
-        axum::Server::bind(&addr)
-            .serve(app.into_make_service())
+        axum::Server::bind(&self.addr)
+            .serve(api_router.into_make_service())
             .await
             .unwrap();
     }
-}
-
-async fn static_handler(uri: Uri) -> impl IntoResponse {
-    let path = uri.path().trim_start_matches('/');
-
-    if path.is_empty() || path == INDEX_HTML {
-        return index_html().await;
-    }
-
-    match Assets::get(path) {
-        Some(content) => {
-            let mime = mime_guess::from_path(path).first_or_octet_stream();
-
-            ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
-        }
-        None => {
-            if path.contains('.') {
-                return not_found().await;
-            }
-            index_html().await
+    async fn output(&self) {
+        info!(
+            "api service enable, listened in: http://{}/api/v1",
+            self.addr
+        );
+        if self.cfg.web.enable {
+            info!("api service web enable, listened in: http://{}", self.addr);
         }
     }
-}
-async fn index_html() -> Response {
-    match Assets::get(INDEX_HTML) {
-        Some(content) => Html(content.data).into_response(),
-        None => not_found().await,
-    }
-}
-
-async fn not_found() -> Response {
-    (StatusCode::NOT_FOUND, "404").into_response()
 }
